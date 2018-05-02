0386470c277d6a1e06d4fe6a7052161e6e0b4e8a
Kyujin Cho <thy2134@gmail.com>
2018-05-02 12:16:16 +0900

    Add cookie creator

--- render.orig.js
+++ render.orig.js
@@ -15,8 +15,65 @@ class App extends React.Component {
 
   render() {
     return (
-      <FileNavigator normal={this.state.normal} secret={this.state.secret} /> 
+      <div className="main">
+        <SetCookie />
+        <FileNavigator normal={this.state.normal} secret={this.state.secret} /> 
+      </div>
     )
+  }
+}
+class SetCookie extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {key: '', value: ''}
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  setCookie(e) { 
+    const d = new Date()
+    d.setMonth(d.getMonth() + 1)
+    const newCookie = `${this.state.key}=${this.state.value}; Expires=${d.toGMTString()};`
+    document.cookie = newCookie
+    console.log(document.cookie)
+    console.log(newCookie)
+    
+    document.getElementById('cookie-notify-msg').className = ''
+    setTimeout(() => {
+      document.getElementById('cookie-notify-msg').className = 'hidden'
+    }, 2000)
+    axios.post('/api/shouldRefresh', this.state)
+      .then((data => {
+        this.setState({
+          key: '',
+          value: ''
+        })
+        document.getElementById('key').value = ''
+        document.getElementById('value').value = ''
+        if(data.data.shouldRefresh) document.location.reload()
+      }).bind(this))
+  }
+  render() {
+    return (
+      <div className="set-cookie">
+        <div>
+          <label htmlFor="key">Key</label>
+          <input type="text" name="key" id="key" onChange={this.onChange.bind(this)} />
+        </div>
+        <div>
+          <label htmlFor="value">Value</label>
+          <input type="text" name="value" id="value" onChange={this.onChange.bind(this)} />
+        </div>
+        <div>
+          <button onClick={this.setCookie.bind(this)}>Set Cookie</button>
+        </div>
+        <span id="cookie-notify-msg" className="hidden">Cookie set</span>
+      </div>
+    )  
   }
 }
 
@@ -32,7 +89,6 @@ class FileNavigator extends React.Component {
       </li>
     ))
     if(this.props.secret.length > 0) {
-      console.log('Concatenating secret files')
       normal = normal.concat(this.props.secret.map((item, index) => (
         <li className="secret" key={index + this.props.normal.length}>
           <a className="file" href={'/api/files/' + item}>{item}</a>
